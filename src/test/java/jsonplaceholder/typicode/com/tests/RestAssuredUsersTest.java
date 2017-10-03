5a9cfa0997ca94972ba709fd977e318d4805ea01
Alena Zubrevich <Alena_Zubrevich@epam.com>
2017-10-03 10:02:35 +0300

    add RequestSpecification to initialize multiple RestAssured url without overriding each other

--- src/test/java/jsonplaceholder/typicode/com/tests/RestAssuredUsersTest.java
+++ src/test/java/jsonplaceholder/typicode/com/tests/RestAssuredUsersTest.java
@@ -1,24 +1,22 @@
 package jsonplaceholder.typicode.com.tests;
 
-import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.builder.RequestSpecBuilder;
 import com.jayway.restassured.response.Response;
+import com.jayway.restassured.specification.RequestSpecification;
 import jsonplaceholder.typicode.com.model.User;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static com.jayway.restassured.RestAssured.given;
 
 public class RestAssuredUsersTest {
 
-    @BeforeTest
-    public void initTest() {
-        RestAssured.baseURI = "http://jsonplaceholder.typicode.com/users";
-    }
+    private final String baseUrl = "http://jsonplaceholder.typicode.com/users";
+    RequestSpecification requestSpecification = new RequestSpecBuilder().setBaseUri(baseUrl).build();
 
     @Test
     public void checkStatusCodeTest() {
-        Response response = given().get().andReturn();
+        Response response = given().spec(requestSpecification).get().andReturn();
         int actualStatusCode = response.statusCode();
 
         Assert.assertEquals(actualStatusCode, 200);
@@ -26,7 +24,7 @@ public class RestAssuredUsersTest {
 
     @Test
     public void checkResponseHeaderTest() {
-        Response response = given().get().andReturn();
+        Response response = given().spec(requestSpecification).get().andReturn();
         String actualContentType = response.header("Content-Type");
 
         Assert.assertEquals(actualContentType, "application/json; charset=utf-8");
@@ -34,7 +32,7 @@ public class RestAssuredUsersTest {
 
     @Test
     public void checkResponseBodyTest() {
-        Response response = given().get().andReturn();
+        Response response = given().spec(requestSpecification).get().andReturn();
         User[] users = response.as(User[].class);
 
         Assert.assertEquals(users.length, 10);
