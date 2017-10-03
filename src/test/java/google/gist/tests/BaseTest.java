d5a9f8bc05db8aea700754dde9f2fd9ff8c5d517
Alena Zubrevich <Alena_Zubrevich@epam.com>
2017-10-03 12:14:09 +0300

    replace RestAssured.baseUrl with RequestedSpecification

--- src/test/java/google/gist/tests/BaseTest.java
+++ src/test/java/google/gist/tests/BaseTest.java
@@ -1,23 +1,23 @@
 package google.gist.tests;
 
-
-import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.builder.RequestSpecBuilder;
 import com.jayway.restassured.specification.RequestSpecification;
 import google.gist.utils.PropertyManager;
-import org.testng.annotations.BeforeTest;
 
 import static com.jayway.restassured.RestAssured.given;
 
 public class BaseTest {
 
     private final String token = PropertyManager.get("git.token");
+    private final String baseUrl = "https://api.github.com/gists";
+    RequestSpecification requestSpecification = new RequestSpecBuilder().setBaseUri(baseUrl).build();
 
-    @BeforeTest
-    public void initTest() {
-        RestAssured.baseURI = "https://api.github.com/gists";
-    }
+//    @BeforeTest
+//    public void initTest() {
+//        RestAssured.baseURI = "https://api.github.com/gists";
+//    }
 
     RequestSpecification getGivenAuth() {
-        return given().auth().oauth2(token);
+        return given().spec(requestSpecification).auth().oauth2(token);
     }
 }
